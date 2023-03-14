b1778a2a871bc5e99d01de754a706db2ae3643b0
shayan1234554321 <kshayan091@gmail.com>
2023-03-14 21:47:27 +0500

    header completed

---
+++ src/modules/importing-images.js
@@ -0,0 +1,8 @@
+// ------------ Imports -----------------
+import logoImg from '../assets/logo.png'
+
+// ------------ constants -----------------
+const logo = document.getElementById('logo')
+
+// ------------ setting src -----------------
+logo.src = logoImg
